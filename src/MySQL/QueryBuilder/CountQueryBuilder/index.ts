98326be390423ffa6e84c65918d90cb7a7f7b09f
RodrigoAES <rodrigoaes.20@hotmail.com>
2025-05-28 10:21:19 -0300

    Added CountSQL to CountQueryBuilder

--- src/MySQL/QueryBuilder/CountQueryBuilder/index.ts
+++ src/MySQL/QueryBuilder/CountQueryBuilder/index.ts
@@ -2,21 +2,16 @@ import { EntityMetadata } from "../../Metadata"
 
 import UnionEntity from "../../UnionEntity"
 
-// Query Builder
-import ConditionalQueryBuilder, {
-    Case,
-    type ConditionalQueryOptions,
-    type CaseQueryOptions,
-} from "../ConditionalQueryBuilder"
-
-import FindQueryBuilder from "../FindQueryBuilder"
+// Query Builders
+import CountSQL from "./CountSQL"
 
 // Helpers
-import { PropertySQLHelper } from "../../Helpers"
+import { SQLStringHelper } from "../../Helpers"
 
 // Types
 import type { EntityTarget } from "../../../types/General"
-import type { CountQueryOptions, CountCaseOptions } from "./types"
+import type { CountQueryOptions } from "./types"
+import type { ConditionalQueryOptions } from "../ConditionalQueryBuilder"
 
 
 export default class CountQueryBuilder<T extends EntityTarget> {
@@ -27,31 +22,16 @@ export default class CountQueryBuilder<T extends EntityTarget> {
     constructor(
         public target: T,
         public options: CountQueryOptions<InstanceType<T>>,
-        alias?: string,
+        alias?: string
     ) {
         this.alias = alias ?? this.target.name.toLowerCase()
         this.metadata = this.loadMetadata()
     }
 
     // Instance Methods =======================================================
-
     // Publics ----------------------------------------------------------------
     public SQL(): string {
-        return Object.entries(this.options)
-            .map(([as, options]) => {
-                switch (typeof options) {
-                    case "string": return `
-                        COUNT (${this.propertySQL(options)}) ${this.asSQL(as)}
-                    `
-
-                    case "object": return `
-                        ${this.handleConditionalCountType(
-                        options
-                    )} ${this.asSQL(as)}
-                    `
-                }
-            })
-            .join(', ')
+        return SQLStringHelper.normalizeSQL(this.countsSQL())
     }
 
     // Privates ---------------------------------------------------------------
@@ -68,65 +48,37 @@ export default class CountQueryBuilder<T extends EntityTarget> {
 
     // ------------------------------------------------------------------------
 
-    private propertySQL(property: string): string {
-        return property === '*'
-            ? property
-            : PropertySQLHelper.pathToAlias(property, this.alias)
+    private countsSQL(): string {
+        return Object.entries(this.options)
+            .map(([as, option]) =>
+                new CountSQL(
+                    this.target,
+                    option,
+                    as,
+                    this.alias,
+                    true
+                )
+                    .SQL()
+            )
+            .join(', ')
     }
 
-    // ------------------------------------------------------------------------
-
-    private asSQL(as: string): string {
-        return `AS ${as}`
-    }
-
-    // ------------------------------------------------------------------------
-
-    private handleConditionalCountType(
-        options: (
-            ConditionalQueryOptions<InstanceType<T>> |
-            CountCaseOptions<InstanceType<T>>
-        )
+    // Static Methods =========================================================
+    // Publics ----------------------------------------------------------------
+    public static countQuery<T extends EntityTarget>(
+        target: T,
+        options: ConditionalQueryOptions<InstanceType<T>>,
+        alias?: string
     ): string {
-        if (Object.getOwnPropertySymbols(options).includes(Case)) return (
-            `COUNT (${this.caseOperatorSQL(
-                options as CountCaseOptions<InstanceType<T>>
-            )})`
+        return SQLStringHelper.normalizeSQL(
+            new CountSQL(
+                target,
+                options,
+                undefined,
+                alias
+            )
+                .SQL()
         )
-
-        else return `(
-            SELECT COUNT(*) 
-            FROM ${this.metadata.tableName}
-            ${this.conditionalSQL(
-            options as ConditionalQueryOptions<InstanceType<T>>
-        )})`
-    }
-
-    // ------------------------------------------------------------------------
-
-    private caseOperatorSQL(options: CountCaseOptions<InstanceType<T>>): (
-        string
-    ) {
-        return ConditionalQueryBuilder.case(
-            this.target,
-            options[Case],
-            undefined,
-            this.alias
-        )
-            .SQL()
-    }
-
-    // ------------------------------------------------------------------------
-
-    private conditionalSQL(
-        options: ConditionalQueryOptions<InstanceType<T>>
-    ): string {
-        return ConditionalQueryBuilder.where(
-            this.target,
-            options,
-            this.alias
-        )
-            .SQL()
     }
 }
 
