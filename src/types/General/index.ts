efff4d623339222c3d1aac7a0eac401315c0f3b1
RodrigoAES <rodrigoaes.20@hotmail.com>
2025-08-12 18:37:58 -0300

    Renamed UnionEntityTarget type to EntityUnionTarget

--- src/types/General/index.ts
+++ src/types/General/index.ts
@@ -5,4 +5,4 @@ export type EntityTarget = new () => BaseEntity
 export type EntityUnionTarget = new (...args: any[]) => BaseEntityUnion<any>
 
 export type AsEntityTarget<T> = Extract<T, EntityTarget>
-export type AsUnionEntityTarget<T> = Extract<T, EntityUnionTarget>
+export type AsEntityUnionTarget<T> = Extract<T, EntityUnionTarget>
